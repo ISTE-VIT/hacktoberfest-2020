c66e91ff0648b89f9ddc995ab417be7ef4b32d42
kull-boii <68529036+kull-boii@users.noreply.github.com>
2020-10-10 22:37:28 +0530

    Optimized Bubble_Sort.cpp
    added flags to make Bubble_sort more faster

--- Sorting Techniques/Bubble_Sort/C++/bubble sort.cpp
+++ Sorting Techniques/Bubble_Sort/C++/bubble sort.cpp
@@ -1,31 +1,43 @@
-#include<iostream>
- 
+#include <iostream>
+// if the array is already sorted then flag vlaue stays 0 and after the first iteration itself the function stops
 using namespace std;
- 
+#define endl '\n'
+
 int main()
 {
-	int a[50],n,i,j,temp;
-	cout<<"Enter the size of array: ";
-	cin>>n;
-	cout<<"Enter the array elements: "; 
-	
-	for(i=0;i<n;++i)
-		cin>>a[i];
-		
-	for(i=1;i<n;++i)
-	{
-		for(j=0;j<(n-i);++j)
-			if(a[j]>a[j+1])
-			{
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
-			}
-	}
-	
-	cout<<"Array after bubble sort:";
-	for(i=0;i<n;++i)
-		cout<<" "<<a[i];
-		
-	return 0;
+    int n;
+    cin >> n;
+    int arr[n];
+    int flag(0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        flag = 0;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+        {
+            // no swaps are done then no need to check for other iterations
+            break;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
