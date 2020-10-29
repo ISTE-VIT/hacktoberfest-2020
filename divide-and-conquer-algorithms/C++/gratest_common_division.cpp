1492972cdaa276d3625762f79a53dc33abf933f0
Georgiy <dev.georgiy@gmail.com>
2020-10-29 21:49:16 +0300

    add gcd src file

---
+++ divide-and-conquer-algorithms/C++/gratest_common_division.cpp
@@ -0,0 +1,18 @@
+// Created by GeorgiyX (github.com/GeorgiyX) 29.10.2020.
+// This is an implementation of Euclid's algorithm for
+// finding the greatest common divisor for two numbers. Recursion free.
+#include <cassert>
+#include <iostream>
+
+size_t getGCD(size_t a, size_t b) {
+    assert(a !=0 || b != 0);
+    if (!a || !b) { return !a ? b : a; } // if one of the numbers is 0 then GCD is another number
+    
+
+}
+
+int main() {
+    return 0;
+}
+
+
