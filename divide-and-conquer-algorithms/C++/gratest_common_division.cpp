ea28f01e1dda8195f85b665e39d1f2fbe9c3bee9
Georgiy <dev.georgiy@gmail.com>
2020-10-29 23:14:19 +0300

    all done

--- divide-and-conquer-algorithms/C++/gratest_common_division.cpp
+++ divide-and-conquer-algorithms/C++/gratest_common_division.cpp
@@ -1,17 +1,28 @@
 // Created by GeorgiyX (github.com/GeorgiyX) 29.10.2020.
-// This is an implementation of Euclid's algorithm for
-// finding the greatest common divisor for two numbers. Recursion free.
 #include <cassert>
 #include <iostream>
 
+// This is an implementation of Euclid's algorithm for
+// finding the greatest common divisor for two numbers. Recursion free.
 size_t getGCD(size_t a, size_t b) {
-    assert(a !=0 || b != 0);
-    if (!a || !b) { return !a ? b : a; } // if one of the numbers is 0 then GCD is another number
-    
+    assert(a != 0 || b != 0); // else - gcd is infinity
+    size_t tmp;
+    while (a) {
+        tmp = a;
+        a = b % a;
+        b = tmp;
+    }
+    return b;
+}
 
+// recursion-based GCD
+size_t getGCDRecursively(size_t a, size_t b) {
+    return a ? getGCDRecursively(b % a, a) : b;
 }
 
 int main() {
+    std::cout << "getGCD " << getGCD(640, 1680) << std::endl;
+    std::cout << "getGCDRecursively " << getGCDRecursively(640, 1680) << std::endl;
     return 0;
 }
 
