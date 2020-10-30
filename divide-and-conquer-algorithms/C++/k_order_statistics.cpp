59822ff4519792d5ca618ac2ab9c0c0200852598
Georgiy <dev.georgiy@gmail.com>
2020-10-30 14:28:26 +0300

    added function descriptions and comments

---
+++ divide-and-conquer-algorithms/C++/k_order_statistics.cpp
@@ -0,0 +1,25 @@
+// Created by GeorgiyX (github.com/GeorgiyX) 30.10.2020.
+#include <iostream>
+
+/**
+ * Selects the pivot element (this is an element with index 0),
+ * then sort the array "arr" - on the left, elements
+ * smaller or equal to the pivot, and on the right, elements
+ * larger. Returns the new pivot index.
+ * @param arr
+ * @param arrSize
+ * @return the new pivot index. The element for this index is the pivot-order statistic.
+ */
+size_t partition(int *arr, size_t arrSize) {
+    size_t pivot = 0;
+    for (size_t i = 0; i < arrSize; ++i) {
+
+    }
+    return pivot;
+}
+
+int kOrderStatistic(int *arr, size_t arrSize, size_t k);
+
+int main() {
+    return 0;
+}
