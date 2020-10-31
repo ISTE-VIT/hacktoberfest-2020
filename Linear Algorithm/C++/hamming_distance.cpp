0987643e955c56bb00f7a5c28a2395ee44aa0989
Georgiy <dev.georgiy@gmail.com>
2020-10-31 23:24:07 +0300

    base struct

---
+++ Linear Algorithm/C++/hamming_distance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/**
+ * Calculates the Hamming distance for two buffers
+ * @param left - 1th buffer
+ * @param right - 2nd buffer
+ * @param size - size of the smallest buffer
+ * @return distance
+ */
+size_t hammingDistance(char *left, char *right, size_t size) {
+    size_t hammingDistance = 0;
+    for (size_t i = 0; i < size; ++i) {
+
+    }
+    return 0;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
